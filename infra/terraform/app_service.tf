ecfdd79f60f7944a056081e92d0f515f1b3c5b49
Matt Braunwart <matt.braunwart@hcahealthcare.com>
2022-02-16 16:28:34 -0500

    Build, Provision, and Deploy!

--- infra/terraform/app_service.tf
+++ infra/terraform/app_service.tf
@@ -2,6 +2,17 @@ resource "random_id" "id" {
   byte_length = 3
 }
 
+locals { 
+  apps = {
+    web = {
+      name = "demo-web-${random_id.id.hex}"
+    }
+    api = {
+      name = "demo-api-${random_id.id.hex}"
+    }
+  }
+}
+
 resource "azurerm_app_service_plan" "asp" {
   name                = "demo-asp"
   location            = azurerm_resource_group.rg.location
@@ -14,13 +25,13 @@ resource "azurerm_app_service_plan" "asp" {
 }
 
 resource "azurerm_app_service" "app" {
-  name                = "demo-appservice-${random_id.id.hex}"
+  forfor_each = local.apps
+  name                = each.value.name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.asp.id
 
   site_config {
     dotnet_framework_version = "v6.0"
-    # scm_type                 = "GitHub"
   }
 }
