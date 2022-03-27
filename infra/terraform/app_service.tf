67c4e05746d2436812cf3e860b94b0c98e559c9b
Matt Braunwart <matt.braunwart@hcahealthcare.com>
2022-03-27 11:52:41 -0400

    testing workflow

--- infra/terraform/app_service.tf
+++ infra/terraform/app_service.tf
@@ -13,25 +13,24 @@ locals {
   }
 }
 
-resource "azurerm_app_service_plan" "asp" {
+resource "azurerm_service_plan" "asp" {
   name                = "demo-asp"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-
-  sku {
-    tier = "Standard"
-    size = "S1"
-  }
+  os_type             = "Linux"
+  sku_name            = "B1"
 }
 
-resource "azurerm_app_service" "app" {
+resource "azurerm_linux_web_app" "app" {
   for_each            = local.apps
   name                = each.value.name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.asp.id
+  service_plan_id     = azurerm_service_plan.asp.id
 
   site_config {
-    dotnet_framework_version = "v6.0"
+    application_stack {
+      dotnet_version = "6.0"
+    }
   }
 }
--- infra/terraform/providers.tf
+++ infra/terraform/providers.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.9"
+      version = "~> 3"
     }
   }
 
@@ -10,7 +10,8 @@ terraform {
     organization = "mbraunwart-test"
 
     workspaces {
-      name = "tf-demo-#{environment}#"
+      # name = "tf-demo-#{environment}#"
+      name = "tf-demo-dev"
     }
   }
 }
